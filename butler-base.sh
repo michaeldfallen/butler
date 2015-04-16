20dc2f9c19d54bb3bc3ebdd23295a6807a91d963
Michael Allen <michael@michaelallen.io>
2015-04-16 14:55:04 +0100

    refactor existing tests to ignore the safety check

--- butler-base.sh
+++ butler-base.sh
@@ -34,10 +34,38 @@ complete_command() {
   done < "$butlerfile"
 }
 
+hash_this() {
+  echo "$@" | gsha256sum | sed 's/  -$//'
+}
+
+record_hash() {
+  local dot_butler="${DOT_BUTLER:-~/.butler}"
+  if [ ! -d "$dot_butler" ]; then
+    mkdir -p "$dot_butler"
+  fi
+  touch "$dot_butler/$@"
+}
+
+can_continue() {
+  local dot_butler="${DOT_BUTLER:-~/.butler}"
+  local name="$1"; shift;
+  local command="$1"; shift;
+  local hash="$(hash_this "$name: $command")"
+  if [ -f "$dot_butler/$hash" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
 execute() {
+  local name="$1";shift;
   local command="$1";shift;
   local args=$@
-  bash -c "$command" -- $args
+  if can_continue "$name" "$command"; then
+    echo "Executing $name: $command"
+    bash -c "$command" -- $args
+  fi
 }
 
 init_butlerfile() {
@@ -69,8 +97,7 @@ run_command() {
     error "found $foundnum commands named $targetname, please fix your butlerfile"
     exit 1
   else
-    echo "Executing $targetname: $foundcommand"
-    execute "$foundcommand" $args
+    execute "$targetname" "$foundcommand" $args
   fi
 }
 
--- tests/helpers.sh
+++ tests/helpers.sh
@@ -14,3 +14,20 @@ rm_tmp() {
   cd $scriptDir
   rm -rf /tmp/git-prompt-tests*
 }
+
+empty_dot_butler() {
+  local dot_butler="${1:-${DOT_BUTLER:-~/.butler}}"
+  rm -r "$dot_butler"
+}
+
+prep_command() {
+  local command="$@"
+  record_test_hash "$command"
+  echo "$command" >> butlerfile
+}
+
+record_test_hash() {
+  export DOT_BUTLER="./.butler_test"
+  hash="$(hash_this "$@")"
+  record_hash "$hash"
+}
